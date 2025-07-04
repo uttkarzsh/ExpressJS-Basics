f3a05ca0b3c215a589f61ca2746d4d64a2439bf6
Uttkarsh Pawan <utkbest@gmail.com>
2025-07-04 07:28:58 +0530

    update logic

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import { fileURLToPath } from "url";
 const app = express();
 const PORT = 4000;
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let user = {
+    userName : "",
+    userEmail : "",
+    userPassword : "",
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +22,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/user", (req, res)=>{
-    res.send(req.body);
+    user = {
+    userName: req.body.name,
+    userEmail: req.body.email,
+    userPassword: req.body.password
+  };
+    res.send("User Added Successfully");
+})
+
+app.delete("/user", (req, res) => {
+  user = {
+    userName: "",
+    userEmail: "",
+    userPassword: ""
+  };
+  res.send("User deleted.");
+});
+
+app.get("/user", (req, res) => {
+    res.send(`The user is ${user.userName}. The email is ${user.userEmail}. The password is ${user.userPassword}.`)
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
